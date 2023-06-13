1508353d92f33353eadd07ad1bbbb1eead045ffa
Samuel <samuelou510@gmail.com>
2023-06-13 09:30:51 -0400

    completed first class lab

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function receivesAFunction(spy){
+  spy();
+}
+
+function returnsANamedFunction(){
+  return function fn(){}
+}
+
+function returnsAnAnonymousFunction(){
+  return function (){}
+}
